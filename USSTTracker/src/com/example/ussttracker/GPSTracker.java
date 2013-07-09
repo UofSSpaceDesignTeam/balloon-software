1938dc2a3e167924e70f4be3bb528fd9a748933a
dylanmilne04@gmail.com <dylanmilne04@gmail.com@b9aa54df-0c64-689f-2dcf-0c738224bd40>
2013-07-09 00:51:53 +0000

    created the gps tracker

---
+++ USSTTracker/src/com/example/ussttracker/GPSTracker.java
@@ -0,0 +1,156 @@
+package com.example.ussttracker;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+
+public class GPSTracker extends Service implements LocationListener 
+{
+	private MainActivity trackerApp;
+	 
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+ 
+    // flag for network status
+    boolean isNetworkEnabled = false;
+ 
+    boolean canGetLocation = false;
+    
+    Location location; // location
+    
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 1 meters
+ 
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000; // 1 second
+ 
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+	
+	public GPSTracker(MainActivity app)
+	{
+		trackerApp = app;
+		getLocation();
+	}
+	
+	public Location getLocation() {
+        try {
+            locationManager = (LocationManager) trackerApp
+                    .getSystemService(LOCATION_SERVICE);
+ 
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+ 
+            if (!isGPSEnabled) {
+                // no network provider is enabled
+            	showSettingsAlert();
+            } else {
+                this.canGetLocation = true;
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                trackerApp.phoneLatitude = location.getLatitude();
+                                trackerApp.phoneLongitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+        return location;
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) 
+	{
+//		trackerApp.phoneLatitude = arg0.getLatitude()*trackerApp.LATITUDE_DEG_TO_KM;
+//		trackerApp.phoneLongitude = trackerApp.LONGITUDE_TO_KMS*Math.cos(arg0.getLatitude()*(Math.PI/180))*(180/Math.PI)*arg0.getLongitude();
+
+	}
+	
+    
+	
+	
+    /**
+     * Function to show settings alert dialog
+     * */
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(trackerApp);
+      
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+  
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+  
+        // Setting Icon to Dialog
+        //alertDialog.setIcon(R.drawable.delete);
+  
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                trackerApp.startActivity(intent);
+            }
+        });
+  
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+            }
+        });
+  
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+	@Override
+	public void onProviderDisabled(String arg0) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
